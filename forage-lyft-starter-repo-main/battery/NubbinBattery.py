b41976e754eb15ab06bc36d39f53f1ef1d6e5fbc
Sam1688 <68300708+Sam1688@users.noreply.github.com>
2023-06-04 06:03:18 +0630

    Delete forage-lyft-starter-repo-main directory

--- forage-lyft-starter-repo-main/battery/NubbinBattery.py
+++
@@ -1,6 +0,0 @@
-from battery.battery import Battery
-
-class NubbinBattery(Battery):
-    def need_Service(self):
-        # Implement the service criteria for NubbinBattery (4 years)
-        return self.current_date.year - self.last_service_date.year >= 4
--- forage-lyft-starter-repo-main/battery/SpindlerBattery.py
+++
@@ -1,8 +0,0 @@
-from battery.battery import Battery
-
-
-class SpindlerBattery(Battery):
-    def need_Service(self):
-        # Implement the service criteria for SpindlerBattery (2 years)
-        return self.current_date.year - self.last_service_date.year >= 2
-    
